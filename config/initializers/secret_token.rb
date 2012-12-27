55831f99eba751eaddfc23b03a1b35de35e6eb65
MathRivest <rivest.mathieu@gmail.com>
2012-12-27 16:34:02 -0500

    remove cscript

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4096f1aebfa9427398085c50cfd287b79324f4dc78dc0c56fb7e7fdc916549e23e1ade8750d89f47a796a71e89c676371c72e18ef41d03e0561c6a080f382ccc'
